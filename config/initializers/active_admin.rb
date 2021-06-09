18efec6faa66ed537365a2b8574394ea606d9a19
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-10 01:14:36 +0900

    通常ログイン無しで管理者画面にログインできるように記述

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -6,6 +6,8 @@ ActiveAdmin.setup do |config|
   #
   config.site_title = "Active Admin Sample"
 
+  config.skip_before_action :authenticate_user!
+
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
